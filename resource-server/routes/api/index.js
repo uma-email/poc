3e0cdfc4a25c96c16af37cab91cd6651b84b2329
1token <zboran@federizer.com>
2020-10-18 09:01:42 +0200

    resource-server removed

--- resource-server/routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-/* const { keycloak } = require('services/Keycloak')
-const AllowMethods = require('middlewares/AllowMethods') */
-
-router.use('/repository', require('repository'))
-
-module.exports = router
