4a73242885d94edd5fa4437f004cb7b42f0e7027
1token <zboran@federizer.com>
2021-04-30 13:01:18 +0200

    verifyToken/iss

--- email-iam/rp-agent-mapper.js
+++ email-iam/rp-agent-mapper.js
@@ -10,23 +10,85 @@
  var HttpRequest = Java.type('org.jboss.resteasy.spi.HttpRequest');
  var JsonWebToken = Java.type('org.keycloak.representations.JsonWebToken');
  var JsonSerialization = Java.type('org.keycloak.util.JsonSerialization');
+ var TokenVerifier = Java.type('org.keycloak.TokenVerifier');
+ var VerifierContext = Java.type('org.keycloak.crypto.AsymmetricSignatureVerifierContext');
+ var Base64 = Java.type('java.util.Base64');
+ 
+ var IDToken = Java.type('org.keycloak.representations.IDToken');
+ var JavaString = Java.type('java.lang.String');
+ 
+ function httpGet(theUrl){
+     var con = new java.net.URL(theUrl).openConnection();
+     con.requestMethod = "GET";
+ 
+     return asResponse(con);
+ }
+ 
+ function asResponse(con){
+     var d = read(con.inputStream);
+ 
+     return {data : d, statusCode : con.responseCode};
+ }
+ 
+ function read(inputStream){
+     var inReader = new java.io.BufferedReader(new java.io.InputStreamReader(inputStream));
+     var inputLine;
+     var response = new java.lang.StringBuffer();
+ 
+     while ((inputLine = inReader.readLine()) !== null) {
+            response.append(inputLine);
+     }
+     inReader.close();
+     return response.toString();
+ }
+ 
+ var verifyToken = function verifyToken(claimsToken) {
+   var verifier = TokenVerifier.create(claimsToken, IDToken.class);
+   var kid = verifier.getHeader().getKeyId();
+   var algorithm = verifier.getHeader().getAlgorithm().name();
+   var iss = verifier.getToken().getIssuer();
+   // print('iss: '  + iss);
+   var response = httpGet(iss).data;
+   // print('response: ' + response);
+   return verifier.getToken().getOtherClaims();
+ }
  
  var httpRequest = keycloakSession.getContext().getContextObject(HttpRequest.class);
+ // print('httpRequest: ' + httpRequest.getDecodedFormParameters());
+ 
  var jwtTicket = httpRequest.getDecodedFormParameters().getFirst("ticket");
  var jwtTickets = httpRequest.getDecodedFormParameters().get("ticket");
+ var claimToken = httpRequest.getDecodedFormParameters().getFirst("claim_token");
+ var claimTokens = httpRequest.getDecodedFormParameters().get("claim_token");
+ 
+ // print('ClaimToken: ' + claimToken);
  
  var parseJwtToken = function parseJwtToken(token) {
      var base64Url = token.split('.')[1];
      var base64Str = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-     var Base64 = Java.type('java.util.Base64');
      var decoded = Base64.getDecoder().decode(base64Str);
-     var JavaString = Java.type('java.lang.String');
      return JSON.parse(new JavaString(decoded));
  }
  
- if (jwtTickets && jwtTickets.size() === 1) {
+ var parseClaimToken = function parseClaimToken(token) {
+     var base64Str = token.replace(/-/g, '+').replace(/_/g, '/');
+     var decoded = Base64.getDecoder().decode(token);
+     return JSON.parse(new JavaString(decoded));
+ }
+ 
+ if ((jwtTickets && jwtTickets.size() === 1) && (claimTokens && claimTokens.size() === 1)) {
      var ticket = parseJwtToken(jwtTicket);
-     var codeVerifier = String(ticket.claims['code-verifier']);
-     // print(codeVerifier);
-     token.setOtherClaims("code-verifier", codeVerifier);
+     var codeVerifier = String(ticket.claims['code_verifier']);
+     // print('codeVerifier: '  + codeVerifier);
+     token.setOtherClaims("code_verifier", codeVerifier);
+ 
+     var claimTokenParsed = parseClaimToken(claimToken);
+     // print('ClaimTokenParsed: ' + claimTokenParsed);
+     var claimsToken = String(claimTokenParsed['claims_token']);
+     // print('ClaimsToken: ' + claimsToken);
+     var claims = verifyToken(claimsToken);
+     print('code_challenge: ' + claims.get('code_challenge'));
+     print('email_address: ' + claims.get('email_address'));
+     token.setOtherClaims("code_challenge", claims.get('code_challenge'));
+     token.setOtherClaims("email_address", claims.get('email_address'));
  }
--- email-iam/rs-service-ticket-code-verifier-policy.js
+++ email-iam/rs-service-ticket-code-verifier-policy.js
@@ -2,25 +2,32 @@ var context = $evaluation.context;
 var identity = context.getIdentity();
 
 var identityAttributes = identity.getAttributes();
-var contextAttributes = context.attributes;
 
-var codeVerifier = identityAttributes.getValue('code-verifier').asString(0);
-var codeChallenge = contextAttributes.getValue('code-challenge').asString(0);
+var codeVerifier = identityAttributes.getValue('code_verifier');
+var codeChallenge = identityAttributes.getValue('code_challenge');
 
-// print(codeVerifier);
-// print(codeChallenge);
+if (codeVerifier && codeChallenge) {
+    var codeVerifierStr = codeVerifier.asString(0);
+    var codeChallengeStr = codeChallenge.asString(0);
 
-var MessageDigest = Java.type('java.security.MessageDigest');
-var JavaString = Java.type('java.lang.String');
-var Base64Url = Java.type('org.keycloak.common.util.Base64Url');
+    // print('codeVerifierStr: ' + codeVerifierStr);
+    // print('codeChallengeStr: ' + codeChallengeStr);
 
-var codeVerifierString = new JavaString(codeVerifier);
-var md = MessageDigest.getInstance("SHA-256");
-var codeVerifierHash = Base64Url.encode(md.digest(codeVerifierString.getBytes('UTF-8')));
-
-if (codeChallenge.localeCompare(codeVerifierHash) === 0) {
-  $evaluation.grant();    
+    var MessageDigest = Java.type('java.security.MessageDigest');
+    var JavaString = Java.type('java.lang.String');
+    var Base64Url = Java.type('org.keycloak.common.util.Base64Url');
+    
+    var codeVerifierString = new JavaString(codeVerifierStr);
+    var md = MessageDigest.getInstance("SHA-256");
+    var codeVerifierHash = Base64Url.encode(md.digest(codeVerifierString.getBytes('UTF-8')));
+    
+    if (codeChallengeStr.localeCompare(codeVerifierHash) === 0) {
+      $evaluation.grant();
+    } else {
+      print('evaluation denied after code verification');    
+      $evaluation.deny();
+    }
+    
 } else {
-  print('evaluation denied after code verification');    
-  $evaluation.deny();
+    $evaluation.deny();
 }
